5e8d9c723999177cc6cd4cb65a3f4ada5720f787
John Unciuleac <unc.john97@gmail.com>
2018-12-11 00:47:38 +0200

    v1

--- PentaStagiu Tema2/Program.cs
+++ PentaStagiu Tema2/Program.cs
@@ -10,6 +10,52 @@ namespace PentaStagiu_Tema2
     {
         static void Main(string[] args)
         {
+            string homework = "Choose 7 methods from the string build-in functionalities and create some examples with them";
+            string add = "only if you want :)";
+            string empty = string.Empty;
+
+            string upper = homework.ToUpper();
+            Console.WriteLine("String-ul uppercase este: " + upper + Environment.NewLine);
+
+            string replace = add.Replace(":)", ";) ;) ;)");
+            Console.WriteLine("Mood: " + replace + Environment.NewLine);
+
+            if(string.IsNullOrEmpty(empty))
+            {
+                Console.WriteLine("E gol!" + Environment.NewLine);
+            }
+
+            int compare = string.Compare(homework, add);
+            if(compare==-1)
+            {
+                Console.WriteLine("Primul string este mai lung!" + Environment.NewLine);
+            }
+            else if(compare==0)
+            {
+                Console.WriteLine("String-urile au aceeasi dimensiune!" + Environment.NewLine);
+            }
+            else
+            {
+                Console.WriteLine("Al doilea string este mai lung!" + Environment.NewLine);
+            }
+
+            int index = homework.IndexOf(7.ToString());
+            Console.WriteLine("7 are index-ul " + (index+1)  + Environment.NewLine);
+
+            string concat = string.Concat(homework, add);
+            Console.WriteLine("String-ul complet este: " + concat + Environment.NewLine);
+
+            bool contains = add.Contains("only");
+            if(contains==true)
+            {
+                Console.WriteLine("String-ul contine substring-ul precizat!" + Environment.NewLine);
+            }
+            else
+            {
+                Console.WriteLine("Nu am gasit string-ul precizat!" + Environment.NewLine);
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ YearAndGender/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YearAndGender
+{
+    class Program
+    {
+        public enum Genders
+        {
+            M=0,
+            F=1
+        }
+
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            int year;
+            int month;
+            int day;
+            string genderUser;
+
+            Console.WriteLine("Introduceti anul:");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduceti luna:");
+            month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduceti ziua:");
+            day = Convert.ToInt32(Console.ReadLine());
+
+            DateTime birthdate = new DateTime(year, month, day);
+            int age = today.Year - birthdate.Year;
+
+            Console.WriteLine("Aveti " + age +" ani :)");
+
+            // In curs de actualizare
+            Genders genderEnum = Genders.M;
+            int numericValue = (int)genderEnum;
+
+            Console.WriteLine("Introduceti genul: ");
+            genderUser = Console.ReadLine();
+
+            if(numericValue==0 && age<=65)
+            {
+                Console.WriteLine("Sunteti barbat, dar nu v-ati pensionat inca. Mai aveti de muncit inca "+ (65-age) +" ani pana la pensionare");
+            }
+            else if(genderUser=="M" && age>=65)
+            {
+                Console.WriteLine("Felicitari, sunteti un barbat pensionar");
+            }
+            if(genderUser=="F" && age <=63)
+            {
+                Console.WriteLine("Sunteti femeie, dar nu v-ati pensionat inca.Mai aveti de muncit inca " + (63 - age) + " ani pana la pensionare");
+            }
+            else if (genderUser == "F" && age >= 63)
+            {
+                Console.WriteLine("Felicitari, sunteti o femeie pensionara");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
